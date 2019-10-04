0aded5c0032f5665cec3f21dbf933cedf9ffa037
Sky JIA <me@skyjia.com>
2019-10-04 12:44:56 +0800

    update gen-id

--- id-gen/id.go
+++ id-gen/id.go
@@ -1,6 +1,7 @@
 package idgen
 
 import (
+	"github.com/google/uuid"
 	"github.com/rs/xid"
 )
 
@@ -8,4 +9,22 @@ import (
 func NewXid() string {
 	guid := xid.New()
 	return guid.String()
+}
+
+// NewSecureRandomUUID 生成一个随机的用于安全验证的 UUID (v4)
+func NewSecureRandomUUID() (string, error) {
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return "", err
+	}
+	return id.String(), nil
+}
+
+// NewUUID 生成一个UUID (v1)
+func NewUUID() (string, error) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return "", err
+	}
+	return id.String(), nil
 }
