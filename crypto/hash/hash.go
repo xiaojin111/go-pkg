49bf3fa2962fb086375abbfb1c6db3d4ed071d66
Sky JIA <me@skyjia.com>
2019-11-05 10:38:01 +0800

    refactor hash

--- crypto/hash/hash.go
+++ crypto/hash/hash.go
@@ -8,48 +8,53 @@ import (
 	"hash"
 )
 
-func hashString(h hash.Hash, in string) string {
+func hashString(h hash.Hash, in string) []byte {
 	return hashBytes(h, []byte(in))
 }
 
-func hashBytes(h hash.Hash, in []byte) string {
+func hashBytes(h hash.Hash, in []byte) []byte {
 	h.Write(in) // nolint: errcheck, gas
 	bs := h.Sum(nil)
-	return fmt.Sprintf("%x", bs)
+	return bs
 }
 
-// SHA256 给定字符串，计返回SHA256十六进制字符串Hash值，其中字母部分为小写字符
-func SHA256(in string) string {
+// HexString 返回字节数组的十六进制字符串，其中字母部分为小写字符
+func HexString(in []byte) string {
+	return fmt.Sprintf("%x", in)
+}
+
+// SHA256String 给定字符串，计返回SHA256 Hash值
+func SHA256String(in string) []byte {
 	h := sha256.New()
 	return hashString(h, in)
 }
 
-// SHA256Bytes 给定 []byte，计返回SHA256十六进制字符串Hash值，其中字母部分为小写字符
-func SHA256Bytes(in []byte) string {
+// SHA256 给定 []byte，计返回SHA256 Hash值
+func SHA256(in []byte) []byte {
 	h := sha256.New()
 	return hashBytes(h, in)
 }
 
-// SHA1 给定字符串，计返回SHA1十六进制字符串Hash值，其中字母部分为小写字符
-func SHA1(in string) string {
+// SHA1String 给定字符串，计返回SHA1 Hash值
+func SHA1String(in string) []byte {
 	h := sha1.New()
 	return hashString(h, in)
 }
 
-// SHA1Bytes 给定 []byte，计返回SHA1十六进制字符串Hash值，其中字母部分为小写字符
-func SHA1Bytes(in []byte) string {
+// SHA1 给定 []byte，计返回SHA1 Hash值
+func SHA1(in []byte) []byte {
 	h := sha1.New()
 	return hashBytes(h, in)
 }
 
-// MD5 给定字符串，计返回MD5十六进制字符串Hash值，其中字母部分为小写字符
-func MD5(in string) string {
+// MD5 给定字符串，计返回MD5 Hash值
+func MD5String(in string) []byte {
 	h := md5.New() // nolint: gas
 	return hashString(h, in)
 }
 
-// MD5Bytes 给定 []byte，计返回MD5十六进制字符串Hash值，其中字母部分为小写字符
-func MD5Bytes(in []byte) string {
+// MD5Bytes 给定 []byte，计返回MD5 Hash值
+func MD5(in []byte) []byte {
 	h := md5.New() // nolint: gas
 	return hashBytes(h, in)
 }
