44303b7c8e128403ff28df37378ab9fdfdacec08
Sky JIA <me@skyjia.com>
2020-02-28 12:25:12 +0800

    refactor log with new methods. removed hijack.

--- log/std.go
+++ log/std.go
@@ -1,24 +1,15 @@
 package log
 
-import (
-	"github.com/sirupsen/logrus"
-)
-
 var (
 	// std is a shared standard Logger
 	std *Logger
 )
 
+func init() {
+	std = NewLogger()
+}
+
 // StandardLogger returns a shared standard Logger.
 func StandardLogger() *Logger {
 	return std
-}
-
-func GetLevel() logrus.Level {
-	return std.GetLevel()
-}
-
-func init() {
-	std = NewLogger()
-	Hijack()
 }
