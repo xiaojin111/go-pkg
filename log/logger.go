e6da3f793305bbe944f7340320af681193b8aa1f
Sky JIA <me@skyjia.com>
2020-03-18 02:50:36 +0800

    added log levels

--- log/logger.go
+++ log/logger.go
@@ -4,11 +4,34 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// Logger is logrus.Logger.
-type Logger = logrus.Logger
+type (
+	// Logger is logrus.Logger.
+	Logger = logrus.Logger
 
-// Level is logrus.Level
-type Level = logrus.Level
+	// Level is logrus.Level
+	Level = logrus.Level
+)
+
+const (
+	// PanicLevel level, highest level of severity. Logs and then calls panic with the
+	// message passed to Debug, Info, ...
+	PanicLevel = logrus.PanicLevel
+	// FatalLevel level. Logs and then calls `logger.Exit(1)`. It will exit even if the
+	// logging level is set to Panic.
+	FatalLevel = logrus.FatalLevel
+	// ErrorLevel level. Logs. Used for errors that should definitely be noted.
+	// Commonly used for hooks to send errors to an error tracking service.
+	ErrorLevel = logrus.ErrorLevel
+	// WarnLevel level. Non-critical entries that deserve eyes.
+	WarnLevel = logrus.WarnLevel
+	// InfoLevel level. General operational entries about what's going on inside the
+	// application.
+	InfoLevel = logrus.InfoLevel
+	// DebugLevel level. Usually only enabled when debugging. Very verbose logging.
+	DebugLevel = logrus.DebugLevel
+	// TraceLevel level. Designates finer-grained informational events than the Debug.
+	TraceLevel = logrus.TraceLevel
+)
 
 // NewLogger creates a new MicroLogger.
 func NewLogger() *Logger {
@@ -17,6 +40,10 @@ func NewLogger() *Logger {
 
 func GetLevel() Level {
 	return std.GetLevel()
+}
+
+func ParseLevel(lvl string) (Level, error) {
+	return logrus.ParseLevel(lvl)
 }
 
 // Log makes use of github.com/go-log/log.Log
